5d1352b0ef605d797f954a60d8610cb1c00e9f53
Joy George Kunjikkuru <joymon@gmail.com>
2021-02-21 11:58:20 -0500

    Added images for 2021-03-02 post

--- joymonscode/2021/03/02/query.sql
+++ joymonscode/2021/03/02/query.sql
@@ -3,40 +3,22 @@ GO
 CREATE DATABASE SCOPED CREDENTIAL ADLSG2Identity
 WITH IDENTITY = 'Managed Identity';
 GO
+-- The location should be blob.core.windows.net cannot be dfs.core.windows.net
 CREATE EXTERNAL DATA SOURCE ADLSG2BLOBDS
 WITH (  
 		TYPE = BLOB_STORAGE,
-		LOCATION   = 'https://adlsgen2testaccount.blob.core.windows.net/csv/',
+		LOCATION   = 'https://<adlsgen2account>.blob.core.windows.net/<container>/',
           CREDENTIAL = ADLSG2Identity
 )
 GO
-TRUNCATE TABLE dbo.fin_L1_TD_Journal
-GO
-BULK INSERT dbo.fin_L1_TD_Journal
-FROM 'fin_L1_TD_Journal.csv/simplecsv.csv'
+BULK INSERT dbo.[table name]
+FROM '<path to .csv> excluding container name'
 WITH 
 	(
 	DATA_SOURCE = 'ADLSG2BLOBDS',
 	FORMAT ='CSV'
 	);
 GO
-SELECT * FROM dbo.fin_L1_TD_Journal
-
+SELECT * FROM dbo.[table name]
+--Below is just to check whether the polybase is enabled or not.
 SELECT SERVERPROPERTY ('IsPolyBaseInstalled') AS IsPolyBaseInstalled;
-
-CREATE DATABASE SCOPED CREDENTIAL SAS1
-WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
-SECRET = 'sv=2020-02-10&sr=d&sig=v38ezkAP5FKqkWDSh3FGD6cCrC9cqmgWjFbvbZclmEY%3D&sdd=1'
-GO
-CREATE EXTERNAL DATA SOURCE ADLSG2DFSDS
-WITH (  
-		TYPE = BLOB_STORAGE,
-		LOCATION   = 'https://adlsgen2testaccount.dfs.core.windows.net/csv/',
-          CREDENTIAL = ADLSG2Identity
-)
-GO
-CREATE EXTERNAL DATA SOURCE SASDS
-WITH (  TYPE = BLOB_STORAGE,  
-		LOCATION   = 'https://adlsgen2testaccount.dfs.core.windows.net/csv/',
-          CREDENTIAL = SAS1
-)
