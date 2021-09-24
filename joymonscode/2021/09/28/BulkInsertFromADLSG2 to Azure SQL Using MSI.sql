43a9cfa1bbb099490231ed219bf803dc06f3b7e7
Joy George Kunjikkuru <joymon@gmail.com>
2021-09-23 22:13:33 -0400

    Added additional files

---
+++ joymonscode/2021/09/28/BulkInsertFromADLSG2 to Azure SQL Using MSI.sql
@@ -0,0 +1,26 @@
+------------------ Droping existing setup -----------------
+DROP EXTERNAL DATA SOURCE ADLSG2BLOBDS
+DROP DATABASE SCOPED CREDENTIAL ADLSG2Identity
+
+------------ Establish connection to ADLS Gen2 ------------
+CREATE DATABASE SCOPED CREDENTIAL ADLSG2Identity
+WITH IDENTITY = 'Managed Identity'
+GO
+
+CREATE EXTERNAL DATA SOURCE ADLSG2BLOBDS
+WITH (
+TYPE = BLOB_STORAGE,
+LOCATION = 'https://<datalake gen2 account>.blob.core.windows.net/<name of the container>/',
+CREDENTIAL = ADLSG2Identity
+)
+GO
+------------ Truncate and perform bulk insert ------------
+TRUNCATE TABLE dbo.companies
+BULK INSERT dbo.companies
+FROM 'companies.csv'
+WITH (
+DATA_SOURCE='ADLSG2BLOBDS',
+FORMAT = 'CSV'
+);
+GO
+SELECT * FROM dbo.companies
