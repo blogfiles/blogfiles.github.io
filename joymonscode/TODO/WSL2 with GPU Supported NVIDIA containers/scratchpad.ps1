0f7a147e60a9a5c03376b457f57067fcf0d5725e
Joy George Kunjikkuru <joymon@gmail.com>
2021-09-27 09:07:12 -0400

    added wsl2 gpu

---
+++ joymonscode/TODO/WSL2 with GPU Supported NVIDIA containers/scratchpad.ps1
@@ -0,0 +1,10 @@
+﻿#https://docs.nvidia.com/cuda/wsl-user-guide/index.html
+#https://docs.nvidia.com/cuda/cuda-installation-guide-microsoft-windows/index.html
+#https://forums.developer.nvidia.com/t/guide-to-run-cuda-wsl-docker-with-latest-versions-21382-windows-build-470-14-nvidia/178365/5
+#https://github.com/NVIDIA/nvidia-container-toolkit
+#https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
+
+docker --version
+docker run --env NVIDIA_DISABLE_REQUIRE=1 --gpus all nvcr.io/nvidia/k8s/cuda-sample:nbody
+docker run --gpus all nvcr.io/nvidia/k8s/cuda-sample:nbody nbody -gpu -benchmark
+docker run --gpus all --rm debian:10-slim nvidia-smi
