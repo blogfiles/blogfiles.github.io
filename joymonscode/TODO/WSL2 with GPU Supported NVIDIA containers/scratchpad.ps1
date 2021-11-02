9d327f3a198a35a5fafb40e6b0df231e6fde5528
Joy George Kunjikkuru <joymon@gmail.com>
2021-11-02 11:26:12 -0400

    Added 01 AAD admin consent

--- joymonscode/TODO/WSL2 with GPU Supported NVIDIA containers/scratchpad.ps1
+++ joymonscode/TODO/WSL2 with GPU Supported NVIDIA containers/scratchpad.ps1
@@ -6,5 +6,17 @@
 
 docker --version
 docker run --env NVIDIA_DISABLE_REQUIRE=1 --gpus all nvcr.io/nvidia/k8s/cuda-sample:nbody
+# Ensure cuda enabled in ubuntu machine
+nvidia-smi
+# Endure its visible to containers
 docker run --gpus all nvcr.io/nvidia/k8s/cuda-sample:nbody nbody -gpu -benchmark
-docker run --gpus all --rm debian:10-slim nvidia-smis
+#My direct command 
+./t-rex -a kawpow -o kp.unmineable.com:3333 -u DOGE:DFmsWrJ32Z4DxT9svLharH3PFpmPowvknK.p620 -p x
+# my Docker command
+docker run -d --name='trex-miner' -p '4067:4067/tcp' -e ALGO=kawpow -e SERVER=kp.unmineable.com:3333 -e WORKER=P620 -e WALLET=DOGE:DFmsWrJ32Z4DxT9svLharH3PFpmPowvknK.p620 --gpus all ptrfrll/nv-docker-trex:cuda11
+docker run -d --name='trex-miner-etchash' -p '4067:4067/tcp' -e ALGO=etchash -e SERVER=etchash.unmineable.com:3333 -e WORKER=P620 -e WALLET=DOGE:DFmsWrJ32Z4DxT9svLharH3PFpmPowvknK.p620 --gpus all ptrfrll/nv-docker-trex:cuda11
+
+#org samplg
+docker run -d --name='trex-miner' -p '4067:4067/tcp' -e ALGO=ethash -e SERVER=stratum+ssl://us2.ethermine.org:5555 -e WORKER=Rig -e WALLET=(myethaddress) --runtime=nvidia ptrfrll/nv-docker-trex
+
+docker run -d --name='trex-miner' -p '4067:4067/tcp' -e ALGO=ethash -e SERVER=stratum+ssl://eu1.ethermine.org:5555 -e WORKER=rig -e WALLET=(myethaddress) --gpus all --env NVIDIA_DISABLE_REQUIRE=1 ptrfrll/nv-docker-trex:cuda11
